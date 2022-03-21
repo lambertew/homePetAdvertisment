10b30fdcab3557cbaa30fb35b67f41c00cb1c0ba
jacobweisbeck <81974631+jacobweisbeck@users.noreply.github.com>
2022-03-21 13:42:49 -0400

    Aesthetics

--- header.php
+++ header.php
@@ -74,7 +74,7 @@
         // 	echo(' | <a href="' . $path . 'logout.php">logout</a><br>');
         // }
         // else {
-        	echo " <br><b>"."Homebase"."</b> ";
+        	echo " <br><b>"."SPCA"."</b> ";
 	        if ($_SESSION['access_level'] >= 1) {
 	        	echo('<a href="' . $path . 'index.php">home</a>');
 	        	echo(' | <a href="' . $path . 'about.php">about</a>');
